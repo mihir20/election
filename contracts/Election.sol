86bba8b2b8e953d153b9c329f622af6978407c88
mihir20 <mihir20121997@gmail.com>
2019-05-23 01:09:32 +0530

    candidates added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,24 @@
 pragma solidity^ 0.5.0;
 
 contract Election {
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint=>Candidate) public candidates;
+
+    uint public candidateCount;
+
     constructor() public{
-        candidate = "Modi";
+        addCandidate("Modi");
+        addCandidate("Rahul");
+    }
+
+    function addCandidate(string memory _name) private{
+        candidateCount++;
+        candidates[candidateCount] = Candidate(candidateCount,_name,0);
     }
 
 }
