99998e3f2ed50c40516403fbe3cf6137d7cddb9e
mihir20 <mihir20121997@gmail.com>
2019-05-23 15:13:11 +0530

    vote function created

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -6,7 +6,7 @@ contract Election {
         string name;
         uint voteCount;
     }
-
+    mapping(address=>bool) public voters;
     mapping(uint=>Candidate) public candidates;
 
     uint public candidateCount;
@@ -19,6 +19,11 @@ contract Election {
     function addCandidate(string memory _name) private{
         candidateCount++;
         candidates[candidateCount] = Candidate(candidateCount,_name,0);
+    }
+
+    function vote(uint _cid) public{
+        voters[msg.sender] = true;
+        candidates[_cid].voteCount++;
     }
 
 }
